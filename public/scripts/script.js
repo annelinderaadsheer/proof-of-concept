32bed1ad819cd28f3af36457f6bc67722318034f
annelinderaadsheer <144004885+annelinderaadsheer@users.noreply.github.com>
2024-06-18 10:28:10 +0200

    View transitions

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,31 +1,35 @@
-// MPA View Transitions!
+// MPA View Transitions
 window.addEventListener("pagereveal", async (e) => {
 
-	// There is an automatic viewTransition, so the user comes from the same origin
+	// Er is een automatische viewTransition, dat betekent dat de gebruiker van dezelfde website komt
 	if (e.viewTransition) {
 
+		// Als de gebruiker niet via een vorige pagina komt, sla de overgang over
 		if (!navigation.activation?.from) {
 			e.viewTransition.skipTransition();
 			return;
 		}
 
+		// Bepaal de overgangsklasse en voeg deze toe aan het documentelement
 		const transitionClass = determineTransitionClass(navigation.activation.from, navigation.currentEntry);
 		document.documentElement.dataset.transition = transitionClass;
 
+		// Wacht tot de overgang voltooid is en verwijder dan de overgangsklasse
 		await e.viewTransition.finished;
 		delete document.documentElement.dataset.transition;
 	}
 
-	// User comes from different origin or did a reload
+	// Gebruiker komt van een andere pagina of heeft een reload uitgevoerd
 	else {
 
-		// Do a reload animation
+		// Voer een reload-animatie uit als het een reloadnavigatie is
 		if (navigation.activation.navigationType == 'reload') {
 			document.documentElement.dataset.transition = "reload";
 			const t = document.startViewTransition(() => {
-				// NOOP
+				// Geen bewerking (NOOP)
 			});
 			try {
+				// Wacht tot de reload-animatie voltooid is en verwijder dan de overgangsklasse
 				await t.finished;
 				delete document.documentElement.dataset.transition;
 			} catch (e) {
@@ -33,7 +37,5 @@ window.addEventListener("pagereveal", async (e) => {
 			}
 			return;
 		}
-
-		// @TODO: manually create a “welcome” viewTransition here?
 	}
 });
